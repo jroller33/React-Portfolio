abdd8953b9621300879b0aca7408dd8e85926125
John Roller <jtroller33@gmail.com>
2022-11-06 22:41:04 -0600

    using cards.js for each project

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,10 +1,28 @@
 import React from "react";
+
+
 import projectsJSON from "../../projectsJSON.json";
+
+
+// using cards for each project that come from projectsJSON
+import Card from "./cards";   
+
+
 
 // add styles for projects here
 const styles = {
 
-}
+  head: {
+
+
+  },
+
+  grid: {
+
+
+  },
+
+};
 
 
 
