d3714ccac01f4917996bc9bf2b8bb5b225236d1d
John Roller <jtroller33@gmail.com>
2022-11-06 22:13:08 -0600

    added profile pic

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -12,7 +12,7 @@ const styles = {    // styles for about me page that are used in html
 
     },
 
-    profilePic: {
+    profilePicStyle: {
 
     },
 
@@ -25,16 +25,13 @@ const styles = {    // styles for about me page that are used in html
 // HTML for about me page goes here
 export default function AboutMe() {
   return (
-
     <section>
-      <h1 style={styles.headerStyle}>About Me</h1>
+      <h1 style={styles.header}>About Me</h1>
       <div>
-        <p style={styles.pStyle}>
+        <p style={styles.paragraph}>
 
           <br />
-          After graduation I plan on purusing a career in full stack
-          development. This bootcamp has been fantastic, the more I learn the
-          more I look forward to starting.
+
         </p>
       </div>
     </section>
