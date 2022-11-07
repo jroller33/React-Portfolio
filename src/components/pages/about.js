0c23e7341b2c0b43565e951041eb5586417f019a
John Roller <jtroller33@gmail.com>
2022-11-06 21:55:39 -0600

    html and css for About Me page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,14 +1,43 @@
 import React from "react";
+// need to import a profile pic
 
-const styles = {    // styles for about me page
+
+const styles = {    // styles for about me page that are used in html
+    
+    header: { 
+
+    },
+
+    paragraph: {
+
+    },
+
+    profilePic: {
+
+    },
+
 
 };
 
+
+// *** add profile pic to html
 
 // HTML for about me page goes here
 export default function AboutMe() {
   return (
 
+    <section>
+      <h1 style={styles.headerStyle}>About Me</h1>
+      <div>
+        <p style={styles.pStyle}>
+
+          <br />
+          After graduation I plan on purusing a career in full stack
+          development. This bootcamp has been fantastic, the more I learn the
+          more I look forward to starting.
+        </p>
+      </div>
+    </section>
 
   );
 }
