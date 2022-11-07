2c7b0ccd9c747cdf82e2a4fe632501dca307195f
John Roller <jtroller33@gmail.com>
2022-11-06 23:44:50 -0600

    all pages loading. links on project page working.

--- src/components/pages/cards.js
+++ src/components/pages/cards.js
@@ -25,25 +25,16 @@ export default function Card(props) {
         </div>
         <div>
           <figcaption style={styles.title}>{props.title}</figcaption>
+          <a  href={props.github}>
           <img
             style={styles.image}
             src={props.image}
             alt="project"
-          ></img>
+           ></img>
+           </a>
         </div>
         <div>
-          <a
-            href={props.github}
-            target="_blank"
-            rel="noreferrer"
-          >
-          </a>
-          <a
-            href={props.link}
-            target="_blank"
-            rel="noreferrer"
-          >
-          </a>
+
         </div>
       </div>
     );
