d3795a374bff9e1b49b084c85bda967e968aa043
John Roller <jtroller33@gmail.com>
2022-11-06 22:53:40 -0600

    working on contact page

---
+++ src/components/pages/contact.js
@@ -0,0 +1,16 @@
+import React, { useState, useRef } from "react";
+
+
+export default function ContactMe() {
+    
+    // styles for contact page
+  const styles = {
+  };
+
+
+    return (
+
+        
+    );
+  }
+  
