7c714c1c85bc84b132700e6c6f51a06a26929530
John Roller <jtroller33@gmail.com>
2022-11-06 22:59:11 -0600

    HTML for contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -4,13 +4,43 @@ import React, { useState, useRef } from "react";
 export default function ContactMe() {
     
     // styles for contact page
-  const styles = {
-  };
+
 
 
     return (
 
-        
+        <div>
+        <h1>Contact Me</h1>
+        <form>
+          <div>
+            <label for="name">Name:</label>
+            <br></br>
+            <input
+              type="text"
+              name="name"
+            />
+          </div>
+          <div>
+            <label for="email">Email:</label>
+            <br></br>
+            <input
+              type="email"
+              name="email"
+            />
+          </div>
+          <div>
+            <label for="message">Message:</label>
+            <br></br>
+            <textarea
+              name="message"
+            />
+          </div>
+          <div>
+            <input type="submit" value="Send Message" />
+          </div>
+        </form>
+      </div>
+
     );
   }
   
