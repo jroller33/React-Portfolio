a3f6d4fc393e415b74260b56689c7cddc53849db
John Roller <jtroller33@gmail.com>
2022-12-23 13:33:49 -0600

    added bootstrap

--- src/components/Divs/Navigation.js
+++ src/components/Divs/Navigation.js
@@ -7,26 +7,24 @@ import "../../styles/Navigation.css";
 //  add links for about, portfolio, contact, and resume pages
 
 export default function Navigation({ newPage }) {
-    return (
+  return (
 
-      <nav class="py-2 bg-light border-bottom">
-    <div class="container d-flex flex-wrap">
-      <ul class="nav me-auto">
-        <li class="nav-item"><a href="#about" onClick={() => newPage("about")} class="nav-link link-dark px-2 active" aria-current="page">About</a></li>
+    <nav class="py-2 bg-light border-bottom">
+      <div class="container d-flex flex-wrap">
+        <ul class="nav me-auto">
+          <li class="nav-item"><a href="#about" onClick={() => newPage("about")} class="nav-link link-dark px-2 active" aria-current="page">About</a></li>
 
-        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Projects</a></li>
+          <li class="nav-item"><a href="#projects" onClick={() => newPage("projects")} class="nav-link link-dark px-2">Projects</a></li>
 
-        <li class="nav-item"><a href="#" class="nav-link link-dark px-2"></a></li>
-        
-        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Contact</a></li>
-        
-        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Resume</a></li>
-      </ul>
+          <li class="nav-item"><a href="#contact" onClick={() => newPage("contact")} class="nav-link link-dark px-2">Contact</a></li>
 
-    </div>
-  </nav>
+          <li class="nav-item"><a href="#resume" onClick={() => newPage("resume")} class="nav-link link-dark px-2">Resume</a></li>
+        </ul>
 
-    );
+      </div>
+    </nav>
+
+  );
 }
 
 /* <nav className="navbar">
