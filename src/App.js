074a21a6bef691abf3853d60f1e34cd7caf534d6
John Roller <jtroller33@gmail.com>
2022-12-23 12:26:18 -0600

    cleared About page, adding bootstrap header and footer

--- src/App.js
+++ src/App.js
@@ -43,12 +43,55 @@ function App() {
   // need to add navbar and footer to this return *****
   return (
     <div className="container">
-      <Header />
       <Navigation thisPage={thisPage} newPage={newPage} /> 
       {pageLoad()}
-      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
+// /* eslint-disable no-unused-vars */
+// import React, { useState, useEffect } from 'react';
+// import Header from "./components/divs/Header";
+// import Navigation from "./components/divs/Navigation";
+// import Footer from "./components/divs/Footer";
+// import { Contact, About, Projects, Resume } from './components/pages'
+// import './App.css';
+// import logo from './logo.svg';
+// function App() {
+//   useEffect(() => {
+//     document.title = "John's React Portfolio";
+//   }, []);
+//   const [thisPage, setThisPage] = useState("about");
+//   // this renders whatever page the user clicks on
+//   function pageLoad() {
+//     switch (thisPage) {
+    
+//       case "projects":
+//         return <Projects />;
+//       case "resume":
+//         return <Resume />;
+//       case "contact":
+//         return <Contact />;
+//       default:
+//         return <About />;
+//     }
+//   }
+//   function newPage(page) {
+//     setThisPage(page);
+//   }
+//   // need to add navbar and footer to this return *****
+//   return (
+//     <div className="container">
+//       <Header />
+//       <Navigation thisPage={thisPage} newPage={newPage} /> 
+//       {pageLoad()}
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -32,22 +32,28 @@ const styles = {    // styles for about me page that are used in html
 };
 
 
-// *** add profile pic to html
-
 // HTML for about me page goes here
 export default function AboutMe() {
   return (
-    <section>
-      <h1 style={styles.header}>About Me</h1>
-      <img src={profilePic} alt="me" style={styles.profilePicStyle}></img>
-
-      <div>
-        <p style={styles.paragraph}>
-            I live in Prattville, Alabama and I am currently a GM Manager at Target. I am in a full-stack coding bootcamp, and after graduation I plan on continuing to learn about software development.
-
-        </p>
-      </div>
-    </section>
-
+    <h1>Hello</h1>
   );
 }
+
+
+// // HTML for about me page goes here
+// export default function AboutMe() {
+//   return (
+//     <section>
+//       <h1 style={styles.header}>About Me</h1>
+//       <img src={profilePic} alt="me" style={styles.profilePicStyle}></img>
+
+//       <div>
+//         <p style={styles.paragraph}>
+//             I live in Prattville, Alabama and I am currently a GM Manager at Target. I am in a full-stack coding bootcamp, and after graduation I plan on continuing to learn about software development.
+
+//         </p>
+//       </div>
+//     </section>
+
+//   );
+// }
